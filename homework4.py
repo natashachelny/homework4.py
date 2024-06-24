bf2c418d3766aa7c16d47ef942859b72964b2df6
natashachelny <natasha-chelny70@mail.ru>
2024-06-24 20:22:56 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,14 @@
+name=input("Столица республики Татарстан: ")
+print("Правильный ответ: ", name)
+
+print("Я тебя люблю".upper())
+
+print("Я ТЕБЯ ЛЮБЛЮ".lower())
+
+print("Я тебя люблю".replace(" ",""))
+
+name="Я тебя люблю"
+print(name[0])
+print(name[-1])
+
+
